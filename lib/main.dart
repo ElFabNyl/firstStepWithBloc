a832a4ee59b716f7c7ad7303673c352df6b5a390
nyll <elfabnyl@gmail.com>
2021-12-06 18:27:43 +0100

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -3,39 +3,24 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:state/logic/cubit/counter_cubit.dart';
 import 'package:state/router/app_router.dart';
 
-
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final _counterCubit = CounterCubit();
+class MyApp extends StatelessWidget {
   final AppRouter _appRouter = AppRouter();
 
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    _counterCubit.close();
-    super.dispose();
-  }
-
+  MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return BlocProvider.value(
-      value: _counterCubit,
+    return BlocProvider(
+      create: (context) => CounterCubit(),
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        onGenerateRoute : _appRouter.onGenerateRoute
+        onGenerateRoute: _appRouter.onGenerateRoute,
       ),
     );
   }
--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:state/logic/cubit/counter_cubit.dart';
+
 import 'package:state/presentation/screen/home_screen.dart';
 import 'package:state/presentation/screen/second_screen.dart';
 import 'package:state/presentation/screen/third_screen.dart';
 
 class AppRouter {
-  final CounterCubit _counterCubit = CounterCubit();
-
   Route<dynamic>? onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case '/':
         return MaterialPageRoute(
-            builder: (_) => BlocProvider.value(
-                  value: _counterCubit,
-                  child: const HomeScreen(title: 'titre', colors: Colors.blue),
-                ));
+            builder: (_) =>
+                const HomeScreen(title: 'titre', colors: Colors.blue));
       case '/second_screen':
         return MaterialPageRoute(builder: (_) => SecondSreen());
       case '/third_screen':
@@ -28,11 +23,5 @@ class AppRouter {
       default:
         return null;
     }
-  }
-
-  //since we've created  the counter cubit manually, we also have to manually close it
-
-  void dispose() {
-    _counterCubit.close();
   }
 }
